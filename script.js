6b1f1540c5750120d422c79d3504c2a23aa1d55c
Naufal Firaas <naufalfiraas09@gmail.com>
2021-04-25 20:35:32 +0700

    finishing sebelum submit

--- script.js
+++ script.js
@@ -6,23 +6,27 @@ const result = document.querySelector('.result-box p');
 
 button.addEventListener('click', (e) => {
   if (cewek.checked) {
-    hasil = hitung(cewek.value, tinggi.value);
+    result.innerHTML = hitung(cewek.value, tinggi.value);
   } else if (cowok.checked) {
-    hasil = hitung(cowok.value, tinggi.value);
+    result.innerHTML = hitung(cowok.value, tinggi.value);
   } else {
     alert('Pilih gender dulu');
   }
-  result.innerHTML = hasil;
   e.preventDefault;
 });
 
 function hitung(gender, tinggi) {
-  if (gender == 'cewek' && tinggi != 0) {
-    const hasil = (tinggi - 100 - (tinggi - 100) * 0.15).toFixed(2);
-    return hasil;
-  } else if (gender == 'cowok' && tinggi != 0) {
-    const hasil = (tinggi - 100 - (tinggi - 100) * 0.1).toFixed(2);
-    return hasil;
+  if (tinggi != 0) {
+    if (gender == 'cewek' && tinggi > 100) {
+      const hasil = (tinggi - 100 - (tinggi - 100) * 0.15).toFixed(2);
+      return hasil;
+    } else if (gender == 'cowok' && tinggi > 100) {
+      const hasil = (tinggi - 100 - (tinggi - 100) * 0.1).toFixed(2);
+      return hasil;
+    } else {
+      alert('Tinggi harus lebih dari 100Cm');
+      return '';
+    }
   } else {
     alert('Masukkan tinggi badan anda');
     return '';
